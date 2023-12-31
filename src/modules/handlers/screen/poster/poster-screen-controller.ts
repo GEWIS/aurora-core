b6baaf89d022a232170c700df01bb13c0839f4b8
Roy Kakkenberg <me@roykakkenberg.nl>
2023-12-31 16:04:01 +0100

    Add endpoint to get NS train departures

--- src/modules/handlers/screen/poster/poster-screen-controller.ts
+++ src/modules/handlers/screen/poster/poster-screen-controller.ts
@@ -2,10 +2,61 @@ import { Controller } from '@tsoa/runtime';
 import {
   Get, Post, Route, Tags,
 } from 'tsoa';
+import axios from 'axios';
 import { PosterScreenHandler } from './poster-screen-handler';
 import HandlerManager from '../../../root/handler-manager';
 import { Screen } from '../../../root/entities';
 import { Poster } from '../../../posters/poster';
+
+enum TrainDepartureStatus {
+  ON_STATION = 'ON_STATION',
+  INCOMING = 'INCOMING',
+}
+
+interface NsTrainResponse {
+  direction: string,
+  name: string,
+  plannedDateTime: string,
+  plannedTimeZoneOffset: number,
+  actualDateTime: string,
+  actualTimeZoneOffset: number,
+  plannedTrack: string,
+  actualTrack: string,
+  product: {
+    number: string,
+    categoryCode: string,
+    shortCategoryName: string,
+    longCategoryName: string,
+    operatorCode: string,
+    operatorName: string,
+    type: string,
+  },
+  trainCategory: string,
+  cancelled: boolean,
+  routeStations: {
+    uicCode: string,
+    mediumName: string,
+  }[],
+  messages: {
+    style: string,
+    message: string,
+  }[],
+  departureStatus: TrainDepartureStatus,
+}
+
+interface TrainResponse {
+  direction: string,
+  plannedDateTime: string,
+  delay: number,
+  trainType: string,
+  operator: string,
+  cancelled: boolean,
+  routeStations: string[],
+  messages: {
+    style: string,
+    message: string,
+  }[],
+}
 
 @Route('screen/poster')
 @Tags('Poster screen')
@@ -35,5 +86,46 @@ export class PosterScreenController extends Controller {
   public async forceUpdatePosters(): Promise<void> {
     await this.screenHandler.posterManager.fetchPosters();
     this.screenHandler.forceUpdate();
+  }
+
+  @Get('train-departures')
+  public async getTrains(): Promise<TrainResponse[]> {
+    const config = {
+      headers: {
+        'Ocp-Apim-Subscription-Key': process.env.NS_KEY,
+      },
+    };
+
+    const departures = (await axios.get('https://gateway.apiportal.ns.nl/reisinformatie-api/api/v2/departures'
+        + '?station=EHV&maxJourneys=40', config)).data.payload.departures as NsTrainResponse[];
+    const minDepartTime = new Date((new Date()).getTime() + 8 * 60000);
+
+    const futureDepartures = departures.filter((departure) => {
+      const actualDepartTime = new Date(departure.actualDateTime);
+      return minDepartTime <= actualDepartTime;
+    });
+
+    return futureDepartures.map((departure): TrainResponse => {
+      let delay = 0;
+      const actualDepartTime = new Date(departure.actualDateTime);
+      if (departure.plannedDateTime !== departure.actualDateTime) {
+        delay = new Date(
+          actualDepartTime.getTime() - new Date(departure.plannedDateTime).getTime(),
+        ).getMinutes();
+      }
+
+      const routeStations: string[] = departure.routeStations.map((s) => s.mediumName);
+
+      return {
+        direction: departure.direction,
+        plannedDateTime: departure.plannedDateTime,
+        delay,
+        trainType: departure.product.categoryCode,
+        operator: departure.product.operatorCode,
+        cancelled: departure.cancelled,
+        routeStations,
+        messages: departure.messages,
+      };
+    });
   }
 }
