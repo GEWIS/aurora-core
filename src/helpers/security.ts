bc754a08c8b744f8b24d4ef8a0a727a01da0907a
Yoronex <me@roykakkenberg.nl>
2024-02-25 17:24:09 +0100

    Fix not ignoring empty roles

--- src/helpers/security.ts
+++ src/helpers/security.ts
@@ -14,10 +14,10 @@ export enum SecurityGroup {
 export function parseRoles(roles: string[]): SecurityGroup[] {
   const rolesToProcess = roles.filter((r) => r !== '');
   const securityGroups: SecurityGroup[] = [];
-  if (roles.includes(process.env.ROLE_ADMIN || '')) securityGroups.push(SecurityGroup.ADMIN);
-  if (roles.includes(process.env.ROLE_BOARD || '')) securityGroups.push(SecurityGroup.BOARD);
-  if (roles.includes(process.env.ROLE_KEY_HOLDER || '')) securityGroups.push(SecurityGroup.KEY_HOLDER);
-  if (roles.includes(process.env.ROLE_BAC || '')) securityGroups.push(SecurityGroup.BAC);
-  if (roles.includes(process.env.ROLE_AVICO || '')) securityGroups.push(SecurityGroup.AVICO);
+  if (rolesToProcess.includes(process.env.ROLE_ADMIN || '')) securityGroups.push(SecurityGroup.ADMIN);
+  if (rolesToProcess.includes(process.env.ROLE_BOARD || '')) securityGroups.push(SecurityGroup.BOARD);
+  if (rolesToProcess.includes(process.env.ROLE_KEY_HOLDER || '')) securityGroups.push(SecurityGroup.KEY_HOLDER);
+  if (rolesToProcess.includes(process.env.ROLE_BAC || '')) securityGroups.push(SecurityGroup.BAC);
+  if (rolesToProcess.includes(process.env.ROLE_AVICO || '')) securityGroups.push(SecurityGroup.AVICO);
   return securityGroups;
 }
