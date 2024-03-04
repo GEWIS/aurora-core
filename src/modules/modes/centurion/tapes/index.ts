3777a9a7d5975a2e3771aa8f3045f2af60fffc34
Roy Kakkenberg <me@roykakkenberg.nl>
2024-03-04 21:03:48 +0100

    Fix formatting & importing issues

--- src/modules/modes/centurion/tapes/index.ts
+++ src/modules/modes/centurion/tapes/index.ts
@@ -1,11 +1,13 @@
 import gebroedersScooterCenturion from './gebroeders-scooter-centurion';
 import gebroedersScooterTotaleEscalatien from './gebroeders-scooter-totale-escalatien';
 import gebroedersScooterTotaleEscalatieNegendarisch from './gebroeders-scooter-totale-escalatie-negendarisch';
+import athenaCenturion from './athena-centurion';
 
 const tapes = [
   gebroedersScooterCenturion,
   gebroedersScooterTotaleEscalatieNegendarisch,
   gebroedersScooterTotaleEscalatien,
+  athenaCenturion,
 ];
 
 export default tapes;
