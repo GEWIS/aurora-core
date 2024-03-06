6e9bedf18f4bb49ef1ca0e61ee631206bfa6fc46
Gijsdeman <github@deman.dev>
2024-03-06 23:04:25 +0100

    Update mock roles

--- src/modules/auth/passport/mock-strategy.ts
+++ src/modules/auth/passport/mock-strategy.ts
@@ -1,14 +1,15 @@
 import { Strategy as CustomStrategy } from 'passport-custom';
 import passport from 'passport';
 import { Request as ExRequest, Response as ExResponse } from 'express';
+import { SecurityGroup } from '../../../helpers/security';
 
 passport.use('mock', new CustomStrategy(
   (req, callback) => {
     // req.login()
 
     callback(null, {
-      name: req.body.name ?? 'mock',
-      roles: req.body.roles ?? ['PRIV - Narrowcasting Test Admin'],
+      name: req.body.name ?? 'dev',
+      roles: req.body.roles ?? [SecurityGroup.ADMIN],
     });
   },
 ));
