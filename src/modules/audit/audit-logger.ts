4dfc1f81ed2f82960f1251067f7ce382eb907a99
Gijsdeman <github@deman.dev>
2024-10-06 18:35:53 +0200

    feat: better express and pino typing
    Latest pino version does not allow for custom typing of the LogFn
    function, as such, the only thing we can do is assert type in the
    AuditLog function.
    
    Express now shadows the global type to extend User, this removes the
    need to explicitly cast any Express request user as AuthUser.

--- src/modules/audit/audit-logger.ts
+++ src/modules/audit/audit-logger.ts
@@ -1,24 +1,37 @@
-import pino from 'pino';
 import logger from '../../logger';
 import AuditService from './audit-service';
 import { AuthUser } from '../auth';
-import LogFn = pino.LogFn;
 
-export function logAudit(user: AuthUser | undefined, msg?: string, ...args: any[]): void {
-  if (!user) {
-    logger.trace(msg!, args);
+/**
+ * Check if the given object is an AuthUser
+ * @param obj - The object to check
+ */
+function isAuthUser(obj: unknown): obj is AuthUser {
+  return (
+    (obj as AuthUser)?.id !== undefined &&
+    typeof (obj as AuthUser)?.id === 'string' &&
+    (obj as AuthUser)?.name !== undefined &&
+    typeof (obj as AuthUser)?.name === 'string'
+  );
+}
+
+/**
+ * Log an audit message
+ * @param user - The user who performed the action
+ * @param msg - The message to log
+ * @param args - Additional arguments to log
+ */
+export function logAudit(user: unknown, msg?: string, ...args: any[]): void {
+  if (!isAuthUser(user)) {
+    logger.error(user, 'Invalid user provided, no id/name present.');
     return;
   }
   if (!msg) return;
   logger.trace<AuthUser>(user, msg, args);
-
-  if (!user.id || !user.name) {
-    logger.error(user, 'Invalid user provided, no id/name present.');
-  }
 
   new AuditService()
     .addLog({ userId: user.id, userName: user.name, action: msg })
     .catch((e) => logger.error(e));
 }
 
-logger.audit = logAudit as LogFn;
+logger.audit = logAudit;
--- src/modules/auth/authentication.ts
+++ src/modules/auth/authentication.ts
@@ -2,6 +2,13 @@ import * as express from 'express';
 import { HttpApiException, HttpStatusCode } from '../../helpers/customError';
 import { AuthUser } from './authUser';
 
+/**
+ * Express middleware to authenticate the user
+ * @param request - The request object
+ * @param securityName - The name of the security scheme
+ * @param scopes - The required scopes
+ * @returns The authenticated user
+ */
 export async function expressAuthentication(
   request: express.Request,
   securityName: string,
@@ -11,15 +18,13 @@ export async function expressAuthentication(
     if (!request.isAuthenticated() || request.user === undefined) {
       throw new HttpApiException(HttpStatusCode.Unauthorized, 'You are not logged in.');
     }
-    const user = request.user as AuthUser;
-
     // Everyone can access, but needs at least one role
-    if (scopes.includes('*') && user.roles.length > 0) {
-      return request.user as AuthUser;
+    if (scopes.includes('*') && request.user.roles.length > 0) {
+      return request.user;
     }
     // Should have one overlapping role/scope
-    if (scopes.some((scope) => user.roles.includes(scope))) {
-      return request.user as AuthUser;
+    if (scopes.some((scope) => request.user!.roles.includes(scope))) {
+      return request.user;
     }
 
     throw new HttpApiException(HttpStatusCode.Forbidden);
