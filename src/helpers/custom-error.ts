f44798f44d164f48914a542269b616a30244ef30
Gijsdeman <github@deman.dev>
2024-10-30 14:27:49 +0100

    docs(custom-error): add explanation of regex.

--- src/helpers/custom-error.ts
+++ src/helpers/custom-error.ts
@@ -8,6 +8,8 @@ export class HttpApiException extends Error {
   public name: string;
 
   constructor(statusCode: HttpStatusCode, message?: string) {
+    // Regex converts status code to space separated format
+    // e.g. ImATeapot => Im A Teapot
     const statusCodeMessage = HttpStatusCode[statusCode].replace(/([A-Z][a-z]*)/g, ' $1').trim();
     super(message ?? statusCodeMessage);
 
