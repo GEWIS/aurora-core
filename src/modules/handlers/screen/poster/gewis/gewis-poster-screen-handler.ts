eb001e58d1c2e299998f1928d512dbfcd231ccc6
Victor Klomp <victor@gewis.nl>
2024-11-28 00:57:28 +0100

    refactor(poster): Move borrel mode to gewis handler

--- src/modules/handlers/screen/poster/gewis/gewis-poster-screen-handler.ts
+++ src/modules/handlers/screen/poster/gewis/gewis-poster-screen-handler.ts
@@ -3,7 +3,47 @@ import { Namespace } from "socket.io";
 
 
 export default class GewisPosterScreenHandler extends BasePosterScreenHandler {
+    private borrelModeDay: number | undefined;
+
+    public borrelModeInterval: NodeJS.Timeout;
+
     constructor(socket: Namespace) {
         super(socket);
+
+        // Check whether we need to enable/disable borrel mode
+        this.borrelModeInterval = setInterval(this.checkBorrelMode.bind(this), 60 * 1000);
+    }
+
+
+    public get borrelMode() {
+        return this.borrelModeDay !== undefined;
+    }
+
+    private checkBorrelMode() {
+        const now = new Date();
+
+        // If borrelmode is enabled, but we arrive at the next day, disable borrelmode again
+        if (this.borrelMode && this.borrelModeDay !== now.getDay()) {
+            this.borrelModeDay = undefined;
+            return;
+        }
+        // if borrelmode is enabled, we do not have to check whether we can enable it
+        if (this.borrelMode) return;
+
+        // By default, enable borrelmode on Thursdays 16:30 local time
+        if (
+            now.getDay() === 4 &&
+            ((now.getHours() === 16 && now.getMinutes() >= 30) || now.getHours() >= 17)
+        ) {
+            this.borrelModeDay = now.getDay();
+        }
+    }
+
+    public setBorrelModeEnabled(enabled: boolean) {
+        if (enabled) {
+            this.borrelModeDay = new Date().getDay();
+        } else {
+            this.borrelModeDay = undefined;
+        }
     }
 }
