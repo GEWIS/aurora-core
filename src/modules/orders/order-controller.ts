22bf9271710e5e990a9f9a398f4854b98fc2af07
Roy Kakkenberg <me@roykakkenberg.nl>
2024-12-18 21:01:11 +0100

    feat: add endpoint to add orders using webhook

--- src/modules/orders/order-controller.ts
+++ src/modules/orders/order-controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Response } from '@tsoa/runtime';
-import { FeatureEnabled } from '../server-settings';
-import { Body, Delete, Get, Post, Route, Security, Tags } from 'tsoa';
+import { Controller, Header, Response, TsoaResponse } from '@tsoa/runtime';
+import { createVerify } from 'crypto';
+import { FeatureEnabled, ServerSettingsStore } from '../server-settings';
+import { Body, Delete, Get, Post, Res, Route, Security, Tags } from 'tsoa';
 import { SecurityNames } from '../../helpers/security';
 import { securityGroups } from '../../helpers/security-groups';
-import OrderStore from './order-store';
 import OrderManager from './order-manager';
+import { OrderSettings } from './order-settings';
 
 interface OrderRequest {
   orderNumber: number;
@@ -15,6 +16,10 @@ interface OrderRequest {
 @Route('/orders')
 @FeatureEnabled('Orders')
 export class OrderController extends Controller {
+  private webhookPublicKey: string;
+
+  private webhookKeyLastUpdate = new Date();
+
   @Security(SecurityNames.LOCAL, securityGroups.orders.base)
   @Get('')
   @Response<string>(409, 'Endpoint is disabled in the server settings')
@@ -26,6 +31,53 @@ export class OrderController extends Controller {
   @Post('')
   @Response<string>(409, 'Endpoint is disabled in the server settings')
   public async addOrder(@Body() orderRequest: OrderRequest) {
+    const manager = OrderManager.getInstance();
+    await manager.addOrder(orderRequest.orderNumber, orderRequest.timeoutSeconds);
+    return manager.getOrders();
+  }
+
+  @Post('webhook')
+  @FeatureEnabled('Orders.WebhookPublicKeyURL')
+  @Response<string>(409, 'Endpoint is disabled in the server settings')
+  public async addOrderWebhook(
+    @Body() orderRequest: OrderRequest,
+    @Header('X-Signature') signature: string,
+    @Res() invalidSignatureResponse: TsoaResponse<400, { message: string }>,
+  ) {
+    const settingsStore = ServerSettingsStore.getInstance();
+    const expiryTimeSeconds = settingsStore.getSetting(
+      'Orders.DefaultTimeoutSeconds',
+    ) as OrderSettings['Orders.DefaultTimeoutSeconds'];
+
+    if (
+      !this.webhookPublicKey ||
+      new Date().getTime() - this.webhookKeyLastUpdate.getTime() > 1000 * expiryTimeSeconds
+    ) {
+      const webhookKeyUrl = settingsStore.getSetting(
+        'Orders.WebhookPublicKeyURL',
+      ) as OrderSettings['Orders.WebhookPublicKeyURL'];
+
+      const response = await fetch(webhookKeyUrl);
+      if (!response.ok) {
+        throw new Error(`Failed to fetch public key: ${response.statusText}`);
+      }
+      this.webhookPublicKey = await response.text();
+    }
+
+    // Create a verifier
+    const verifier = createVerify('RSA-SHA256');
+    verifier.update(JSON.stringify(orderRequest));
+    verifier.end();
+
+    // Decode the Base64 signature
+    const decodedSignature = Buffer.from(signature, 'base64');
+
+    // Verify the signature
+    const isValid = verifier.verify(this.webhookPublicKey, decodedSignature);
+    if (!isValid) {
+      return invalidSignatureResponse(400, { message: 'Signature invalid' });
+    }
+
     const manager = OrderManager.getInstance();
     await manager.addOrder(orderRequest.orderNumber, orderRequest.timeoutSeconds);
     return manager.getOrders();
--- src/modules/orders/order-settings.ts
+++ src/modules/orders/order-settings.ts
@@ -1,9 +1,13 @@
 export interface OrderSettings {
   Orders: boolean;
   'Orders.DefaultTimeoutSeconds': number;
+  'Orders.WebhookPublicKeyURL': string;
+  'Orders.WebhookPublicKeyExpirySeconds': number;
 }
 
 export const OrderSettingsDefault: OrderSettings = {
   Orders: true,
   'Orders.DefaultTimeoutSeconds': 120,
+  'Orders.WebhookPublicKeyURL': '',
+  'Orders.WebhookPublicKeyExpirySeconds': 60 * 60,
 };
