81dcd73f08f451e319997c45a1b2a3bcde23e25e
Victor Klomp <victor@gewis.nl>
2024-12-28 14:18:04 +0100

    style(poster): fix exports/imports

--- src/modules/handlers/screen/poster/index.ts
+++
@@ -1,3 +0,0 @@
-// eslint-disable-next-line import/no-cycle -- TODO fix cyclic dependency
-export { BasePosterScreenController } from './base-poster-screen-controller';
-export { default as PosterScreenHandler } from './base-poster-screen-handler';
