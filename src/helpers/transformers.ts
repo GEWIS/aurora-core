cc57e11e1a4c16cb06bc492088f0a06bb960b5e9
Roy Kakkenberg <me@roykakkenberg.nl>
2025-04-25 19:06:28 +0200

    fix: writing null to DB if default setting evaluates to false

--- src/helpers/transformers.ts
+++ src/helpers/transformers.ts
@@ -3,11 +3,11 @@ import { ColumnOptions } from 'typeorm';
 export function jsonTransformer<T>(): ColumnOptions['transformer'] {
   return {
     from(value: string | null): T | null {
-      if (!value) return null;
+      if (value == null) return null;
       return JSON.parse(value);
     },
     to(value: T | null): string | null {
-      if (!value) return null;
+      if (value == null) return null;
       return JSON.stringify(value);
     },
   };
