e0e78ce5a25e74407830b0fbf1e72514e9cbbd6d
Roy Kakkenberg <38660000+Yoronex@users.noreply.github.com>
2025-10-15 20:12:08 +0200

    feat: improve RandomEffectsHandler to use patterns and switch colors (#170)
    * feat: improve RandomEffectsHandler to use patterns and switch colors
    
    * fix: RandomEffectsHandler switching effects just after song segment

--- src/modules/handlers/lights/random-effects-handler.ts
+++ src/modules/handlers/lights/random-effects-handler.ts
@@ -1,24 +1,72 @@
 import { LightsGroup } from '../../lights/entities';
-import { BeatEvent } from '../../events/music-emitter-events';
+import { BeatEvent } from '../../events';
 import { BeatFadeOut, Sparkle, StaticColor, Wave } from '../../lights/effects/color';
 import { getTwoComplementaryRgbColors, RgbColor } from '../../lights/color-definitions';
 import EffectsHandler from './effects-handler';
 import { SearchLight, TableRotate } from '../../lights/effects/movement';
+import {
+  getRandomLightsEffectDirection,
+  getRandomLightsEffectPattern,
+} from '../../lights/effects/lights-effect-pattern';
 
 export default class RandomEffectsHandler extends EffectsHandler {
   private lastSectionStart: number = 0;
 
   private colors: RgbColor[] = getTwoComplementaryRgbColors().colorNames;
 
+  private effectSwitchTimeout: NodeJS.Timeout | null = null;
+
   // Override entity register function to assign random effect
   public registerEntity(entity: LightsGroup) {
+    // If this is the first entity for this handler, schedule the next effect switch
+    if (this.entities.length === 0) this.scheduleNextEffectSwitch();
+
     super.registerEntity(entity);
     this.assignRandomEffect(entity);
+  }
+
+  public removeEntity(entityCopy: LightsGroup) {
+    super.removeEntity(entityCopy);
+
+    // If there are no more entities left, remove the effect switch
+    if (this.entities.length === 0 && this.effectSwitchTimeout !== null) {
+      clearTimeout(this.effectSwitchTimeout);
+    }
   }
 
   private updateRandomColor() {
     const colors = getTwoComplementaryRgbColors();
     this.colors = colors.colorNames;
+  }
+
+  /**
+   * Create a timeout that will change the effects on all entities.
+   * Switch is between 20 and 40 seconds.
+   * @private
+   */
+  private scheduleNextEffectSwitch() {
+    // Clear any existing effect timeout if present
+    if (this.effectSwitchTimeout !== null) clearTimeout(this.effectSwitchTimeout);
+
+    const timeoutTime = Math.round(Math.random() * 20000 + 20000);
+    this.effectSwitchTimeout = setTimeout(this.performEffectSwitch.bind(this), timeoutTime);
+  }
+
+  /**
+   * Assign a new effect including new colors to all entities.
+   * Also, schedule a new switch between 20 and 40 seconds from now.
+   * @private
+   */
+  private performEffectSwitch() {
+    // Remove the timeout, because it has fired.
+    this.effectSwitchTimeout = null;
+
+    this.updateRandomColor();
+    this.entities.forEach((entity) => {
+      this.assignRandomEffect(entity);
+    });
+
+    this.scheduleNextEffectSwitch();
   }
 
   /**
@@ -30,15 +78,27 @@ export default class RandomEffectsHandler extends EffectsHandler {
     // Destroy the existing effect(s)
     this.clearEffect(entity);
 
+    const pattern = getRandomLightsEffectPattern();
+    const direction = getRandomLightsEffectDirection();
+
     if (entity.movingHeadWheels.length === 0) {
       const random = Math.random();
       if (random < 0.8) {
         this.groupColorEffects.set(
           entity,
-          new BeatFadeOut(entity, { colors: this.colors, enableFade: false, nrBlacks: 1 }),
+          new BeatFadeOut(entity, {
+            colors: this.colors,
+            enableFade: false,
+            nrBlacks: 1,
+            pattern,
+            direction,
+          }),
         );
       } else if (random < 0.9) {
-        this.groupColorEffects.set(entity, new Wave(entity, { colors: this.colors }));
+        this.groupColorEffects.set(
+          entity,
+          new Wave(entity, { colors: this.colors, pattern, direction }),
+        );
       } else {
         this.groupColorEffects.set(entity, new Sparkle(entity, { colors: this.colors }));
       }
@@ -60,6 +120,9 @@ export default class RandomEffectsHandler extends EffectsHandler {
       this.entities.forEach((group) => {
         this.assignRandomEffect(group);
       });
+
+      // Schedule a new effect timeout, because we have just switched.
+      this.scheduleNextEffectSwitch();
     }
 
     super.beat(event);
